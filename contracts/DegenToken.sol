4aed721a57417ef8083b42cc2f14b49dcb15db7b
u-Kuro <rcdupitas@gmail.com>
2023-07-14 15:11:19 +0800

    updated

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -8,7 +8,23 @@ contract DegenToken is ERC20, Ownable {
 
     constructor() ERC20("Degen", "DGN") {}
 
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    function mintTokens(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function transferTokens(address recipient, uint256 amount) external {
+        _transfer(msg.sender, recipient, amount);
+    }
+
+    function redeemTokens(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function burnTokens(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function checkBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
     }
 }
